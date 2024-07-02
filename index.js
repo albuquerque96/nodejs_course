f7063c25107d39090561e628fd8fa5b5d94b82ec
albuquerque96 <88738051+albuquerque96@users.noreply.github.com>
2024-07-02 20:22:26 +0100

    before the module code is executed, node js will wrap it with a function wrapper that provides module scope this saves us from having to horry about conflicting varibles or fucntions there is proper encapsulation and resuability in unaffected

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-const addfn = require("./add.js")
-console.log("hello from index.js");
-const sum = addfn(1,2)
-const sum2 = addfn(2,3)
-console.log(sum);
-console.log(sum2);
+require("./batman")
+require("./superman")
