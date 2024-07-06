e1948c7ecc43bf97c5377028658bb2166ccce80c
albuquerque96 <88738051+albuquerque96@users.noreply.github.com>
2024-07-06 15:01:48 +0100

    buffer overflow

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const buffer = new Buffer.from("Vishwas");//UTF-8 encoding by default
 
+
+buffer.write("code")//buffer still contains info related to previous data 
+//buffer.write("Code Evolution")//bigger than buffer capacity extra carachters discarded
+console.log(buffer.toString());
 console.log(buffer);//hexadecimal representation of binary data 
 console.log(buffer.toJSON());//decimal representation of binary data 
