990cf89818612a637452fa230775b938f991eaf7
albuquerque96 <88738051+albuquerque96@users.noreply.github.com>
2024-07-08 14:46:48 +0100

    thread pool

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const fs = require("node:fs/promises")
+const crypto = require("node:crypto");
+const start = Date.now();
 
-async function readFile() {
-    try {
-        const data = await fs.readFile("file.txt", "utf8")
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-
-}
-
-readFile()
-
+crypto.pbkdf2Sync("password","salt",100000,512,"sha512")
+crypto.pbkdf2Sync("password","salt",100000,512,"sha512")
+crypto.pbkdf2Sync("password","salt",100000,512,"sha512")
+console.log("Hash: ",Date.now()-start);
 /*const fs = require("node:fs");
 
 
